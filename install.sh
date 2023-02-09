85010ed060bf9e5311d96fd86e421e6617c3e756
ottagit <ottaosawa@gmail.com>
2023-02-09 05:31:14 +0000

    install docker on image

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 #Install nginx
 sudo amazon-linux-extras install nginx1
 
-sudo mkdir -p /var/www/website
-sudo sh -c "echo '<html><h1>This is a website</h1></html>' > /var/www/website/index.html"
+#Install Docker
+sudo yum -y install docker
--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh -x
 
 #Run nginx at boot
-
 sudo systemctl enable nginx
+
+#Run docker at boot
+sudo systemctl enable docker
+#Create the docker group
+sudo groupadd docker
+#Add your user to the docker group
+sudo usermod -aG docker $USER
+#Activate changes to groups
